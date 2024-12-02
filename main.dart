899b259306fb04e9f4b2701a527e739431402681
Jilaajaa123 <zainalgsclasher@gmail.com>
2024-12-02 08:43:04 +0700

    Add files via upload
    AAS RPL

--- main.dart
+++ main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -8,79 +10,137 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Flutter Layout Demo'),
-          centerTitle: true,
+          title: Text('Evaluasi Akhir'),
         ),
-        body: SingleChildScrollView(
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            Image.asset(
-              'jepang.jpg',
-              height: 250,
-              width: double.infinity,
-              fit: BoxFit.cover,
-            ),
-
-            Padding(
-              padding: const EdgeInsets.all(16.0),
+        body: Row(
+          children: [
+            
+            Container(
+              width: 300, 
+              padding: EdgeInsets.all(16.0),
+              color: const Color.fromARGB(255, 208, 230, 19),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    'Wisata Tokyo',
+                    'Resep Khas Mas Amba',
                     style: TextStyle(
                       fontSize: 24,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-                  SizedBox(height: 8),
-                  Text(
-                    'Gunung Fuji, Japan',
-                    style: TextStyle(fontSize: 16),
+                  SizedBox(height: 16),
+                  Container(
+                    padding: EdgeInsets.all(16.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: const Color.fromARGB(255, 117, 88, 8)),
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Nasi Goreng Spesial',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 8),
+                        Text(
+                          'Nasi Goreng adalah salah satu makanan paling populer di Indonesia. Sebagai hidangan yang sangat sederhana dan mudah dibuat, nasi goreng terbuat dari nasi yang digoreng dengan bumbu-bumbu seperti bawang merah, bawang putih, kecap manis, dan rempah lainnya. Biasanya, nasi goreng juga ditambahkan bahan pelengkap seperti telur, ayam, sosis, udang, atau sayuran.',
+                        ),
+                        //ini buat bintang sama review
+                        SizedBox(height: 16),
+                        Row(
+                          children: [
+                            Icon(Icons.star),
+                            Icon(Icons.star),
+                            Icon(Icons.star),
+                            Icon(Icons.star),
+                            Icon(Icons.star),
+                            SizedBox(width: 8),
+                            Text('1K Reviews'),
+                          ],
+                        ),
+                        SizedBox(height: 16),
+                        Row(
+                          children: [
+                            Container(
+                              padding: EdgeInsets.all(8.0),
+                              decoration: BoxDecoration(
+                                border: Border.all(color: const Color.fromARGB(255, 244, 76, 54)),
+                                borderRadius: BorderRadius.circular(8.0),
+                              ),
+                              //ini icon 3 nya
+                              child: Column(
+                                children: [
+                                  Icon(Icons.kitchen, color: const Color.fromARGB(255, 244, 54, 54)),
+                                  SizedBox(height: 8),
+                                  Text('PREP'),
+                                  Text('5 min'),
+                                ],
+                              ),
+                            ),
+                            SizedBox(width: 16),
+                            Container(
+                              padding: EdgeInsets.all(8.0),
+                              decoration: BoxDecoration(
+                                border: Border.all(color: const Color.fromARGB(255, 36, 170, 36)),
+                                borderRadius: BorderRadius.circular(8.0),
+                              ),
+                              child: Column(
+                                children: [
+                                  Icon(Icons.timer, color: Colors.green),
+                                  SizedBox(height: 8),
+                                  Text('COOK'),
+                                  Text('5-10 min'),
+                                ],
+                              ),
+                            ),
+                            SizedBox(width: 16),
+                            Container(
+                              padding: EdgeInsets.all(8.0),
+                              decoration: BoxDecoration(
+                                border: Border.all(color: const Color.fromARGB(255, 0, 68, 255)),
+                                borderRadius: BorderRadius.circular(8.0),
+                              ),
+                              child: Column(
+                                children: [
+                                  Icon(Icons.restaurant, color: const Color.fromARGB(255, 0, 110, 255)),
+                                  SizedBox(height: 8),
+                                  Text('FEEDS:'),
+                                  Text('1'),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
             ),
-
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Row(
-                children: [
-                  Icon(Icons.star, color: Colors.amber),
-                  SizedBox(width: 8),
-                  Text('4.1'),
-                  SizedBox(width: 16),
-                  ElevatedButton.icon(
-                    onPressed: () {},
-                    icon: Icon(Icons.phone),
-                    label: Text('CALL'),
-                  ),
-                  SizedBox(width: 16),
-                  ElevatedButton.icon(
-                    onPressed: () {},
-                    icon: Icon(Icons.navigation),
-                    label: Text('ROUTE'),
-                  ),
-                  SizedBox(width: 16),
-                  ElevatedButton.icon(
-                    onPressed: () {},
-                    icon: Icon(Icons.share),
-                    label: Text('SHARE'),
-                  ),
-                ],
+            // foto
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(16.0),
+                child: Image.asset(
+                  'jusjstrawberry.jpg',
+                  fit: BoxFit.cover, 
+                  height: double.infinity, 
+                ),
               ),
             ),
-
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                'Nikmati akhir tahunmu dengan liburan ke Japan dengan trevel kami, di jamin nyaman aman terpercaya.',
-                style: TextStyle(fontSize: 16),
-              ),
-            ),
-          ]),
+          ],
         ),
       ),
     );
   }
 }
+
+
+
+
+
