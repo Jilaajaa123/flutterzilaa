7afdf397cfcbaffef55d899e024c3cd1a641b7bc
Jilaajaa123 <zainalgsclasher@gmail.com>
2025-02-12 11:03:14 +0700

    Add files via upload

---
+++ gradient_container.dart
@@ -0,0 +1,41 @@
+import 'package:acakdadu/dice_roller.dart';
+import 'package:flutter/material.dart';
+
+// Konstanta untuk posisi awal dan akhir gradient
+const startAlignment = Alignment.topLeft; // Posisi awal gradient di pojok kiri atas
+const endAlignment = Alignment.bottomRight; // Posisi akhir gradient di pojok kanan bawah
+
+// Kelas GradientContainer untuk membuat container dengan gradient
+class GradientContainer extends StatelessWidget {
+  // Konstruktor untuk membuat objek GradientContainer
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  // Variabel untuk warna pertama dan kedua gradient
+  final Color color1; // Warna pertama gradient
+  final Color color2; // Warna kedua gradient
+
+  @override
+  // Fungsi untuk membuat widget
+  Widget build(context) {
+    // Membuat container dengan gradient
+    return Container(
+      // Dekorasi untuk container 
+      decoration: BoxDecoration(
+        // Membuat gradient
+        gradient: LinearGradient(
+          // Daftar warna untuk gradient
+          colors: [color1, color2],
+          // Posisi awal gradient
+          begin: startAlignment,
+          // Posisi akhir gradient
+          end: endAlignment,
+        ),
+      ),
+      // Anak widget dari container
+      child: Center(
+        // Membuat widget DiceRoller di tengah-tengah container
+        child: DiceRoller(),
+      ),
+    );
+  }
+}     
--- main.dart
+++ main.dart
@@ -1,146 +1,26 @@
 import 'package:flutter/material.dart';
+import 'gradient_container.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  // conts membuat widget di simpan di memory, sehingga tidak perlu  buat widget baru setiap aplikasim dipanggil
+  runApp(
+    MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Evaluasi Akhir'),
-        ),
-        body: Row(
-          children: [
-            
-            Container(
-              width: 300, 
-              padding: EdgeInsets.all(16.0),
-              color: const Color.fromARGB(255, 208, 230, 19),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Resep Khas Mas Amba',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(height: 16),
-                  Container(
-                    padding: EdgeInsets.all(16.0),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: const Color.fromARGB(255, 117, 88, 8)),
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Nasi Goreng Spesial',
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(height: 8),
-                        Text(
-                          'Nasi Goreng adalah salah satu makanan paling populer di Indonesia. Sebagai hidangan yang sangat sederhana dan mudah dibuat, nasi goreng terbuat dari nasi yang digoreng dengan bumbu-bumbu seperti bawang merah, bawang putih, kecap manis, dan rempah lainnya. Biasanya, nasi goreng juga ditambahkan bahan pelengkap seperti telur, ayam, sosis, udang, atau sayuran.',
-                        ),
-                        //ini buat bintang sama review
-                        SizedBox(height: 16),
-                        Row(
-                          children: [
-                            Icon(Icons.star),
-                            Icon(Icons.star),
-                            Icon(Icons.star),
-                            Icon(Icons.star),
-                            Icon(Icons.star),
-                            SizedBox(width: 8),
-                            Text('1K Reviews'),
-                          ],
-                        ),
-                        SizedBox(height: 16),
-                        Row(
-                          children: [
-                            Container(
-                              padding: EdgeInsets.all(8.0),
-                              decoration: BoxDecoration(
-                                border: Border.all(color: const Color.fromARGB(255, 244, 76, 54)),
-                                borderRadius: BorderRadius.circular(8.0),
-                              ),
-                              //ini icon 3 nya
-                              child: Column(
-                                children: [
-                                  Icon(Icons.kitchen, color: const Color.fromARGB(255, 244, 54, 54)),
-                                  SizedBox(height: 8),
-                                  Text('PREP'),
-                                  Text('5 min'),
-                                ],
-                              ),
-                            ),
-                            SizedBox(width: 16),
-                            Container(
-                              padding: EdgeInsets.all(8.0),
-                              decoration: BoxDecoration(
-                                border: Border.all(color: const Color.fromARGB(255, 36, 170, 36)),
-                                borderRadius: BorderRadius.circular(8.0),
-                              ),
-                              child: Column(
-                                children: [
-                                  Icon(Icons.timer, color: Colors.green),
-                                  SizedBox(height: 8),
-                                  Text('COOK'),
-                                  Text('5-10 min'),
-                                ],
-                              ),
-                            ),
-                            SizedBox(width: 16),
-                            Container(
-                              padding: EdgeInsets.all(8.0),
-                              decoration: BoxDecoration(
-                                border: Border.all(color: const Color.fromARGB(255, 0, 68, 255)),
-                                borderRadius: BorderRadius.circular(8.0),
-                              ),
-                              child: Column(
-                                children: [
-                                  Icon(Icons.restaurant, color: const Color.fromARGB(255, 0, 110, 255)),
-                                  SizedBox(height: 8),
-                                  Text('FEEDS:'),
-                                  Text('1'),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            // foto
-            Expanded(
-              child: Container(
-                padding: EdgeInsets.all(16.0),
-                child: Image.asset(
-                  'jusjstrawberry.jpg',
-                  fit: BoxFit.cover, 
-                  height: double.infinity, 
-                ),
-              ),
-            ),
-          ],
+        body: Container(
+          // decoration: const BoxDecoration(
+          //   gradient: LinearGradient(
+          //     colors: [
+          //     ],
+          //     begin: Alignment.topLeft,
+          //     end: Alignment.bottomRight,
+          //   ),
+          // ),
+          child: const GradientContainer(
+                Color.fromARGB(255,41,24,71),
+                Color.fromARGB(255, 173, 56, 194)
+            )
         ),
       ),
-    );
-  }
+    ),
+  );
 }
-
-
-
-
-
